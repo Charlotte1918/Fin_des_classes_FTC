b6a5d492cc68e9bedf9aa4d71fe3927de81802dd
Zest1918 <charlottedube28@gmail.com>
2026-06-01 11:15:23 -0400

    made Mr.Pratt tutorial

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadTest.java
@@ -12,6 +12,18 @@ public class GamePadTest extends OpMode {
 
     @Override
     public void loop() {
-        
+        double speedForward = -gamepad1.left_stick_y / 2.0;
+        double difX = gamepad1.left_stick_x - gamepad1.left_stick_y;
+        double sumTriggers = gamepad1.right_trigger + gamepad1.left_trigger;
+
+        telemetry.addData("left_x", gamepad1.left_stick_x);
+        telemetry.addData("left_y", speedForward);
+        telemetry.addData("right_x", gamepad1.right_stick_x);
+        telemetry.addData("right_y", gamepad1.right_stick_y);
+        telemetry.addData("a button", gamepad1.a);
+        telemetry.addData("b button", gamepad1.b);
+        telemetry.addData("dif_x", difX);
+        telemetry.addData("sum_triggers", sumTriggers);
+
     }
 }
