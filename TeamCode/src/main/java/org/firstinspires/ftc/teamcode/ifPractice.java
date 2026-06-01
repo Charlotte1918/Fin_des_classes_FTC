e6a007493dda2d37f8ca9fd8c9fd879d5321aa12
Zest1918 <charlottedube28@gmail.com>
2026-06-01 11:37:06 -0400

    made Mr.Pratt tutorial ifPractice

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ifPractice.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp
+public class ifPractice extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+        boolean aButton = gamepad1.a;
+
+        if (aButton) {
+            telemetry.addData("A Button", "Pressed !");
+        } else {
+            telemetry.addData("A Button", "NOT pressed !");
+        }
+
+        telemetry.addData("A Button State", aButton);
+
+    }
+}
